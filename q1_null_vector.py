4e691ba27cace5b2ffba55788014ca3d556668d0
Robins Mehta <robinsmehta7@gmail.com>
2025-05-13 15:27:33 +0545

    add qnum1

---
+++ q1_null_vector.py
@@ -0,0 +1,10 @@
+#Create a null vector of size 10 but the fifth value which is 1.
+import numpy as np
+
+# Create a null vector of size 10
+vector = np.zeros(10)
+
+# Set the fifth element (index 4) to 1
+vector[4] = 1
+
+print(vector)
